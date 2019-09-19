20af6439e75f7446943a1585b40a4739aa6b7261
noursirfan <noursirfan@gmail.com>
2019-09-19 14:21:15 +0700

    thrid

--- luas_app.cpp
+++ luas_app.cpp
@@ -1,12 +1,21 @@
+/** Nama   : Irfan Kurniawan
+*** NIM    : 181011450114
+********************************/
+
 #include<iostream>
 
 using namespace std;
 
-void persegi(int a, int b);
-void segitiga(int a, int b);
-void lingkaran(int a);
+void luas_persegi(int a, int b);
+void luas_segitiga(int a, int b);
+void luas_lingkaran(int a);
 
 main() {
+    menu:
+    system("cls");
+
+    char pilihan;
+
     int pilih,
     P, L,
     A, T,
@@ -22,46 +31,54 @@ main() {
 
     if(pilih == 1) {
 
-        cout<<"Luas Persegi \n";
-        cout<< "Panjang "; cin>>P;
-        cout<<"Lebar "; cin>>L;
+        cout<<"Hitung Luas Persegi \n";
+        cout<<"Masukan Panjang "; cin>>P;
+        cout<<"Masukan Lebar "; cin>>L;
 
-        persegi(P, L);
+        luas_persegi(P, L);
 
     }else if(pilih == 2){
 
-        cout<<"Luas Segitiga \n";
-        cout<<"Alas "; cin>>A;
-        cout<<"Tinggi "; cin>>T;
+        cout<<"Hitung Luas Segitiga \n";
+        cout<<"Masukan Alas "; cin>>A;
+        cout<<"Masukan Tinggi "; cin>>T;
 
-        segitiga(A, T);
+        luas_segitiga(A, T);
 
     }else if(pilih == 3) {
 
-        cout<<"HituLingkaran \n";
-        cout<<"MenghituLingkaran"<<endl;
-        cout<<"Jari-jari "; cin>>J;
+        cout<<"Hitung Luas Lingkaran \n";
+        cout<<"Menghitung Luas Lingkaran"<<endl;
+        cout<<"Masukan Jari-jari "; cin>>J;
 
-        lingkaran(J);
+        luas_lingkaran(J);
 
-    }else {
+    }else if(pilih == 4) {
         exit(0);
+    }else {
+        cout<<"Pilihan tidak ada \n";
+        cout<<"Apakah anda ingin kembali ke menu: [Y/T] \n"; cin>>pilihan;
+        if(pilihan == 'Y') {
+            goto menu;
+        }else{
+            exit(0);
+        }
     }
 }
 
-void persegi(int P, int L) {
+void luas_persegi(int P, int L) {
     int Luas;
     Luas = P * L;
     cout<<"Luas: = "<<Luas<<endl;    
 }
 
-void segitiga(int A, int T) {
+void luas_segitiga(int A, int T) {
     int Luas;
     Luas = 0.5*A*T;
     cout<<"Luas: = "<<Luas<<endl;    
 }
 
-void lingkaran(int J) {
+void luas_lingkaran(int J) {
     int Luas;
     Luas = 3.14*J*J;
     cout<<"Luas: = "<<Luas<<endl;    
